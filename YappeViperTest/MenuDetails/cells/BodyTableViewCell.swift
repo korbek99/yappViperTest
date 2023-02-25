36285ce0cf6d92f8a1cc9eb97e4cd975d1cb9ce8
JosePreatorianWise <preatorianwise99@hotmail.com>
2023-02-25 20:51:29 -0300

    adding tableviewcells

---
+++ YappeViperTest/MenuDetails/cells/BodyTableViewCell.swift
@@ -0,0 +1,75 @@
+//
+//  BodyTableViewCell.swift
+//  YappeViperTest
+//
+//  Created by Jose David Bustos H on 25-02-23.
+//
+
+
+import UIKit
+struct BodyTableViewModel {
+    let urlImg: String
+    
+    init(urlImg: String) {
+        self.urlImg = urlImg
+    }
+}
+class BodyTableViewCell: UITableViewCell {
+ 
+    override func prepareForReuse() {
+    }
+    // MARK: - IBOutlets
+    lazy var imgMenuDetail: UIImageView = {
+        let image: UIImageView = .init()
+        image.contentMode = .scaleAspectFit
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = 20.0
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    // MARK: - init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUIUX()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(_ model: BodyTableViewModel) {
+        if let imageURL = URL(string: model.urlImg) {
+       
+                let data = try? Data(contentsOf: imageURL)
+                if let data = data {
+                    let image = UIImage(data: data)
+                    imgMenuDetail.image =  image
+                }
+        }
+    }
+    
+    // MARK: - Functions
+    func setupUIUX() {
+        self.backgroundColor = .white
+        addSubview(imgMenuDetail)
+        
+        imgMenuDetail.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
+        imgMenuDetail.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15).isActive = true
+        imgMenuDetail.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15).isActive = true
+        imgMenuDetail.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 5).isActive = true
+        
+        imgMenuDetail.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        imgMenuDetail.widthAnchor.constraint(equalToConstant: 250).isActive = true
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
+
